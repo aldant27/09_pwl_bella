14ba42385737a2adaaff7e0443cb002bb11a246e
aldant yafi <aldantyafi6@gmail.com>
2022-04-08 23:13:57 +0700

    make relation to matakuliah on model Mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model; //Model Eloquent
 //use App\Models\Mahasiswa;
+use App\Models\Kelas;
+use App\Models\MataKuliah;
 
 class Mahasiswa extends Model
 {
@@ -31,6 +33,11 @@ class Mahasiswa extends Model
 
     public function kelas(){
         return $this->belongsTo(Kelas::class);
+    }
+
+    public function matakuliah(){
+        return $this->belongsToMany(MataKuliah::class, 'mahasiswa_matakuliah', 'mahasiswa_id', 'matakuliah_id')
+            ->withPivot('nilai');
     }
 
 }
